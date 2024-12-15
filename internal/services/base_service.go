a6cb5630ba585961fd96b02dc02470b738855fa6
sahandMac <sahandmac@sahandMacs-MacBook-Pro.local>
2024-12-15 23:34:51 +0330

    Fix some code in CRUD section and Complete comments.

--- internal/services/base_service.go
+++ internal/services/base_service.go
@@ -30,53 +30,55 @@ type Preload struct {
 	string
 }
 
-func NewBaseService[T any, Tc any, Tu any, Tr any](cfg *config.Config) *BaseService[T, Tc, Tu, Tr] {
+// NewBaseService initializes a new BaseService instance.
+func NewBaseService[T any, Tc any, Tu any, Tr any](cfg *config.Config, preloads []Preload) *BaseService[T, Tc, Tu, Tr] {
 	return &BaseService[T, Tc, Tu, Tr]{
 		Database: db.GetDB(),
 		Log:      logger.NewLogger(cfg),
+		Preloads: preloads,
 	}
 }
 
-// Create
+// Create inserts a new record in the database and returns the created entity.
 func (s *BaseService[T, Tc, Tu, Tr]) Create(ctx context.Context, req *Tc) (*Tr, error) {
-	model, _ := utils.TypeConverter[T](req)
-	tx := s.Database.WithContext(ctx).Begin()
+	model, _ := utils.TypeConverter[T](req)   // Convert request to model type.
+	tx := s.Database.WithContext(ctx).Begin() // Begin a new database transaction.
 	if err := tx.Create(model).Error; err != nil {
-		tx.Rollback()
+		tx.Rollback() // Rollback transaction in case of error.
 		s.Log.Error(logger.Potgres, logger.Insert, nil, err.Error())
 		return nil, err
 	}
-	tx.Commit()
+	tx.Commit() // Commit the transaction.
 	u, _ := utils.TypeConverter[models.BaseModel](model)
-	return s.GetById(ctx, u.ID)
+	return s.GetById(ctx, u.ID) // Retrieve the created entity by ID.
 }
 
-// Update
+// Update modifies an existing record based on the provided ID and request data.
 func (s *BaseService[T, Tc, Tu, Tr]) Update(ctx context.Context, id int, req *Tu) (*Tr, error) {
-	updateMap, _ := utils.TypeConverter[map[string]interface{}](req)
+	updateMap, _ := utils.TypeConverter[map[string]interface{}](req) // Convert request to map.
 	snakeMap := make(map[string]interface{})
 	for k, v := range *updateMap {
-		snakeMap[utils.ToSnakeCase(k)] = v
+		snakeMap[utils.ToSnakeCase(k)] = v // Convert keys to snake_case.
 	}
 	snakeMap["modified_by"] = &sql.NullInt64{Valid: true, Int64: int64(ctx.Value(constatns.UserIdKey).(float64))}
 	snakeMap["modified_at"] = &sql.NullTime{Valid: true, Time: time.Now().UTC()}
 
 	model := new(T)
-	tx := s.Database.WithContext(ctx).Begin()
+	tx := s.Database.WithContext(ctx).Begin() // Begin transaction.
 	if err := tx.Model(model).
-		Where("id = ? AND deleted_by is null", id).
-		Updates(*updateMap).Error; err != nil {
+		Where(constatns.NotNullQuery, id).
+		Updates(snakeMap).Error; err != nil {
 		tx.Rollback()
 		s.Log.Error(logger.Potgres, logger.Update, nil, err.Error())
 		return nil, err
 	}
-	tx.Commit()
-	return s.GetById(ctx, id)
+	tx.Commit()               // Commit transaction.
+	return s.GetById(ctx, id) // Retrieve the updated entity by ID.
 }
 
-// Delete
+// Delete performs a soft delete by setting the `deleted_by` and `deleted_at` fields.
 func (s *BaseService[T, Tc, Tu, Tr]) Delete(ctx context.Context, id int) error {
-	tx := s.Database.WithContext(ctx).Begin()
+	tx := s.Database.WithContext(ctx).Begin() // Begin transaction.
 	model := new(T)
 	userId := ctx.Value(constatns.UserIdKey)
 	if userId == nil {
@@ -87,43 +89,43 @@ func (s *BaseService[T, Tc, Tu, Tr]) Delete(ctx context.Context, id int) error {
 		"deleted_at": &sql.NullTime{Valid: true, Time: time.Now().UTC()},
 	}
 	if count := tx.Model(model).
-		Where("id = ? AND deleted_by is null", id).
+		Where(constatns.NotNullQuery, id).
 		Updates(deleteMap).RowsAffected; count == 0 {
 		s.Log.Error(logger.Potgres, logger.Update, nil, serviceerrors.RecordNotFound)
 		tx.Rollback()
 		return &serviceerrors.ServiceError{EndUserMessage: serviceerrors.RecordNotFound}
 	}
-	tx.Commit()
+	tx.Commit() // Commit transaction.
 	return nil
 }
 
-// GetById
+// GetById retrieves a record by its ID if not deleted.
 func (s *BaseService[T, Tc, Tu, Tr]) GetById(ctx context.Context, id int) (*Tr, error) {
-	tx := s.Database.WithContext(ctx).Begin()
+	tx := s.Database.WithContext(ctx).Begin() // Begin transaction.
 	model := new(T)
 
 	if err := tx.Model(model).
-		Where("id = ? AND deleted_by is null", id).
+		Where(constatns.NotNullQuery, id).
 		First(model).Error; err != nil {
 		s.Log.Error(logger.Potgres, logger.Select, nil, err.Error())
 		tx.Rollback()
 		return nil, err
 	}
 
-	return utils.TypeConverter[Tr](model)
+	return utils.TypeConverter[Tr](model) // Convert model to response type.
 }
 
-// GetByFilter
+// GetByFilter retrieves records based on filters with pagination.
 func (s *BaseService[T, Tc, Tu, Tr]) GetByFilter(ctx context.Context, req *dto.PaginationInputWithFilter) (*dto.PagedList[Tr], error) {
 	return pagination[T, Tr](req, s.Preloads, s.Database)
 }
 
-// GetQuery
+// getQuery generates the SQL WHERE clause based on dynamic filters.
 func getQuery[T any](filter *dto.DynaimcFilter) string {
 	model := new(T)
 	modelType := reflect.TypeOf(*model)
 	query := make([]string, 0)
-	query = append(query, "deleted_by is null")
+	query = append(query, "deleted_by is null") // Ensure only non-deleted records are fetched.
 
 	if filter.Filter != nil {
 		for name, filter := range *&filter.Filter {
@@ -175,10 +177,10 @@ func getQuery[T any](filter *dto.DynaimcFilter) string {
 		}
 	}
 
-	return strings.Join(query, " AND ")
+	return strings.Join(query, " AND ") // Combine conditions with AND.
 }
 
-// GetSort
+// getSort generates the SQL ORDER BY clause based on dynamic sorting options.
 func getSort[T any](filter *dto.DynaimcFilter) string {
 	model := new(T)
 	modelType := reflect.TypeOf(*model)
@@ -192,10 +194,10 @@ func getSort[T any](filter *dto.DynaimcFilter) string {
 			}
 		}
 	}
-	return strings.Join(sort, ", ")
+	return strings.Join(sort, ", ") // Combine sorting options with commas.
 }
 
-// Paginate
+// pagination handles pagination, filtering, and sorting logic for queries.
 func pagination[T any, Tr any](req *dto.PaginationInputWithFilter, preloads []Preload, db *gorm.DB) (*dto.PagedList[Tr], error) {
 	model := new(T)
 	items := new([]T)
@@ -203,13 +205,13 @@ func pagination[T any, Tr any](req *dto.PaginationInputWithFilter, preloads []Pr
 	var totalRows int64 = 0
 	var err error
 
-	db = preload(db, preloads)
-	query := getQuery[T](&req.DynaimcFilter)
-	sort := getSort[T](&req.DynaimcFilter)
+	db = preload(db, preloads)               // Apply preloads.
+	query := getQuery[T](&req.DynaimcFilter) // Generate query conditions.
+	sort := getSort[T](&req.DynaimcFilter)   // Generate sorting options.
 
 	db.Model(model).
 		Where(query).
-		Count(&totalRows)
+		Count(&totalRows) // Count total matching records.
 
 	if err = db.Model(model).
 		Where(query).
@@ -220,25 +222,26 @@ func pagination[T any, Tr any](req *dto.PaginationInputWithFilter, preloads []Pr
 		return nil, err
 	}
 
-	itemResponse, err = utils.TypeConverter[[]Tr](items)
+	itemResponse, err = utils.TypeConverter[[]Tr](items) // Convert items to response type.
 
-	return getPageList(itemResponse, totalRows, req.PageNumber, req.PageSize), err
+	return getPageList(itemResponse, totalRows, req.GetPageNumber(), req.GetPageSize()), err
 }
 
+// getPageList creates a PagedList object with metadata for pagination.
 func getPageList[T any](items *[]T, totalRow int64, pageNumber int, pageSize int) *dto.PagedList[T] {
 	pl := &dto.PagedList[T]{
 		PageNumber: pageNumber,
 		TotalRows:  totalRow,
 		Items:      items,
 	}
-	pl.TotalPages = int(math.Ceil(float64(totalRow) / float64(pageSize)))
-	pl.HasNextPage = pl.TotalPages > pageNumber
-	pl.HasPerviousPage = pageNumber > 1
+	pl.TotalPages = int(math.Ceil(float64(totalRow) / float64(pageSize))) // Calculate total pages.
+	pl.HasNextPage = pl.TotalPages > pageNumber                           // Check if next page exists.
+	pl.HasPerviousPage = pageNumber > 1                                   // Check if previous page exists.
 
 	return pl
 }
 
-// Preload
+// preload applies preload options to the database query.
 func preload(db *gorm.DB, preloads []Preload) *gorm.DB {
 	for _, item := range preloads {
 		db = db.Preload(item.string)
