351b380a1dca89f85a97f0bdf24201c3b5a88a93
sahandMac <sahandmac@sahandMacs-MacBook-Pro.local>
2024-12-21 18:12:35 +0330

    Fix some property and propertyCategory fields.

--- internal/models/property.go
+++ internal/models/property.go
@@ -2,18 +2,18 @@ package models
 
 type PropertyCategory struct {
 	BaseModel
-	Name       string     `gorm:"size:15;type:string;not null; unique"`
-	Icon       string     `gorm:"size:1000;type:string;not null; unique"`
+	Name       string     `gorm:"size:50;type:string;not null; unique"`
+	Icon       string     `gorm:"size:1000;type:string;not null;"`
 	Properties []Property `gorm:"foreignKey:CategoryId"`
 }
 
 type Property struct {
 	BaseModel
-	Name        string           `gorm:"size:15;type:string;not null; unique"`
-	Icon        string           `gorm:"size:1000;type:string;not null; unique"`
+	Name        string           `gorm:"size:50;type:string;not null; unique"`
+	Icon        string           `gorm:"size:1000;type:string;not null;"`
 	Category    PropertyCategory `gorm:"foreignKey:CategoryId;constraint:OnDelete:No Action;OnUpdate:No Action"`
 	CategoryId  int
 	Description string `gorm:"size:1000;type:string;not null; unique"`
-	DataType    string `gorm:"size:15;type:string;not null; unique"`
-	Unit        string `gorm:"size:15;type:string;not null; unique"`
+	DataType    string `gorm:"size:15;type:string;not null;"`
+	Unit        string `gorm:"size:15;type:string;not null;"`
 }
