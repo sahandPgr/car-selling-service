c3c5e58815070c8c8457e3d5ba4494eff8189687
sahandMac <sahandmac@sahandMacs-MacBook-Pro.local>
2024-12-23 18:04:03 +0330

    Add Car Model dtos.

--- api/dto/car.go
+++ api/dto/car.go
@@ -5,7 +5,7 @@ type CreateCarTypeRequest struct {
 }
 
 type UpdateCarTypeRequest struct {
-	Name string `json:"name" binding:"alpha,min=3,max=15"`
+	Name string `json:"name,omitempty" binding:"alpha,min=3,max=15"`
 }
 
 type CarTypeResponse struct {
@@ -18,10 +18,32 @@ type CreateGearboxRequest struct {
 }
 
 type UpdateGearboxRequest struct {
-	Name string `json:"name" binding:"alpha,min=3,max=15"`
+	Name string `json:"name,omitempty" binding:"alpha,min=3,max=15"`
 }
 
 type GearboxResponse struct {
 	Id   int    `json:"id"`
 	Name string `json:"name"`
+}
+
+type CreateCarModelRequest struct {
+	Name      string `json:"name" binding:"required,min=3,max=15"`
+	CompanyId int    `json:"companyId" binding:"required"`
+	CarTypeId int    `json:"carTypeId" binding:"required"`
+	GearboxId int    `json:"gearboxId" binding:"required"`
+}
+
+type UpdateCarModelRequest struct {
+	Name      string `json:"name,omitempty"`
+	CompanyId int    `json:"companyId,omitempty"`
+	CarTypeId int    `json:"carTypeId,omitempty"`
+	GearboxId int    `json:"gearboxId,omitempty"`
+}
+
+type CarModelResponse struct {
+	Id      int             `json:"id"`
+	Name    string          `json:"name"`
+	Company CompanyResponse `json:"company"`
+	CarType CarTypeResponse `json:"carType"`
+	Gearbox GearboxResponse `json:"gearbox"`
 }
